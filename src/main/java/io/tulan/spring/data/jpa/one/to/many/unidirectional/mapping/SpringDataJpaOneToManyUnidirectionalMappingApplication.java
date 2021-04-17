f26a48268bac2523eb81dea65406243fc8936065
dennis masinde <chengwadennismasinde@gmail.comgit config --global user.email chengwadennismasinde@gmail.comgit config --global user.email chengwadennismasinde@gmail.com>
2021-04-17 12:14:31 +0300

    add app properties for db connection

---
+++ src/main/java/io/tulan/spring/data/jpa/one/to/many/unidirectional/mapping/SpringDataJpaOneToManyUnidirectionalMappingApplication.java
@@ -0,0 +1,13 @@
+package io.tulan.spring.data.jpa.one.to.many.unidirectional.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaOneToManyUnidirectionalMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaOneToManyUnidirectionalMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tulan/spring/data/jpa/one/to/many/unidirectional/mapping/SpringDataJpaOneToManyUnidirectionalMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tulan.spring.data.jpa.one.to.many.unidirectional.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaOneToManyUnidirectionalMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
